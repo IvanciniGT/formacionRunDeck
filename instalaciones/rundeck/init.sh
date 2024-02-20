abee4e2c41cb1a355789326f2d2da2bf65d7925d
Ubuntu <ubuntu@ip-172-31-34-201.eu-west-1.compute.internal>
2024-02-20 15:24:09 +0000

    bd

--- instalaciones/rundeck/init.sh
+++ instalaciones/rundeck/init.sh
@@ -2,4 +2,5 @@ mkdir -p ~/environment/data/rundeck
 mkdir -p ~/environment/data/mariadb
 chmod 777 ~/environment/data/rundeck
 chmod 777 ~/environment/data/mariadb
+export PUBLIC_IP=$(curl ifconfig.me)
 docker compose up -d
